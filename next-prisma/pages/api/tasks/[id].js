338478409883e95c037cefacc92fbc6bd83877e7
TechNightFury <onni.tan62@gmail.com>
2024-11-18 16:56:08 -0800

    feat: made main api features

---
+++ next-prisma/pages/api/tasks/[id].js
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler (req, res) {
+    const taskId = parseInt(req.query.id);
+
+    // GET ONE DATA
+    if (req.method === 'GET') {
+        const task = await prisma.task.findFirst({
+            where: {
+                id: taskId
+            }
+        });
+
+        res.json(task);
+    }
+
+    // UPDATE DATA
+    if (req.method === 'POST') {
+        const {title, description} = req.body;
+        const updatedTask = await prisma.task.update({
+            where: {
+                id: taskId
+            },
+            data: {
+                title,
+                description
+            }
+        });
+
+        res.json(updatedTask);
+    }
+    // DELETE DATA
+    else if (req.method === 'DELETE') {
+        const deletedTask = await prisma.task.delete({
+            where: { id: taskId }
+        });
+
+        
+        res.json(deletedTask);       
+    }
+    else {
+        res.status(405).json({
+            error: 'Method Not Allowed'
+        });
+    }
+}
